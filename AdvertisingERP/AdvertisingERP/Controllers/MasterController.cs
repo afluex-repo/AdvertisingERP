84bb37f027535cadb1414bf9ecccd9a22b9456b4
afluex-reena <afluexdevelopment@gmail.com>
2022-04-07 17:53:17 +0530

    Changes

---
+++ AdvertisingERP/AdvertisingERP/Controllers/MasterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdvertisingERP.Controllers
+{
+    public class MasterController : Controller
+    {
+        // GET: Master
+        public ActionResult Site()
+        {
+            return View();
+        }
+    }
+}
