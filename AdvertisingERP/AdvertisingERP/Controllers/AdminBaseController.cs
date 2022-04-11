49a8eaaba4f906925f8cc4018eaf5dff19650c8f
ashwani-afluex <developer6.afluex@gmail.com>
2022-04-11 18:56:51 +0530

    change

--- AdvertisingERP/AdvertisingERP/Controllers/AdminBaseController.cs
+++ AdvertisingERP/AdvertisingERP/Controllers/AdminBaseController.cs
@@ -10,39 +10,49 @@ namespace AdvertisingERP.Controllers
     public class AdminBaseController : Controller
     {
         //
-        // GET: /Base/
 
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            // code involving this.Session // edited to simplify
             HttpSessionStateBase session = filterContext.HttpContext.Session;
-            // If the browser session or authentication session has expired...
-            if (session.IsNewSession || Session["UserID"] == null)
+            if (session != null && session["UserID"] == null)
             {
-                Session["UserID"] = "1";
-                //filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(
-                //     new { action = "Index", Controller = "Home" }));
+                //filterContext.Result = new RedirectToRouteResult(
+                //    new RouteValueDictionary {
+                 RedirectToAction("Login", "Home");   
+                                //});
             }
-            else
-            {
-                //var data=(List<Login>) Session["LoginUser"];
-                //if (data[0].UserType == 1)
-                //{
-                //    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(
-                //      new { action = "Index", Controller = "Admin" }));
-                //}
-                //else if (data[0].UserType == 3)
-                //{
-                //    filterContext.Result = new RedirectResult("http://www.google.com", true);                 
-                //}
-                //else
-                //{
-                //    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(
-                //      new { action = "Index", Controller = "Home" }));
-                //}
-            }
-            base.OnActionExecuting(filterContext); // re-added in edit
         }
+    //{
+    //    // code involving this.Session // edited to simplify
+    //    HttpSessionStateBase session = filterContext.HttpContext.Session;
+    //    // If the browser session or authentication session has expired...
+    //    if (session.IsNewSession || Session["UserID"] == null)
+    //    {
 
-    }
+    //        filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(
+    //             new { action = "Login", Controller = "Home" }));
+    //    }
+    //    else
+    //    {
+    //        //var data=(List<Login>) Session["LoginUser"];
+    //        //if (data[0].UserType == 1)
+    //        //{
+    //        //    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(
+    //        //      new { action = "Index", Controller = "Admin" }));
+    //        //}
+    //        //else if (data[0].UserType == 3)
+    //        //{
+    //        //    filterContext.Result = new RedirectResult("http://www.google.com", true);                 
+    //        //}
+    //        //else
+    //        //{
+    //        //    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(
+    //        //      new { action = "Index", Controller = "Home" }));
+    //        //}
+    //    }
+    //    base.OnActionExecuting(filterContext); // re-added in edit
+
+    //}
+
+}
 }
